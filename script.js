3b0f90528fa2ee349a7d336c27e031bc9574bc0e
Genevieve Suder <genevievesuder@gmail.com>
2022-11-08 19:31:54 -0800

    added text effects and buttons

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 //const h2 = document.createElement("h2");
 //h2.textContent = "This content added by JavaScript";
 //document.querySelector("body").appendChild(h2);
+
+const character = "Kuromi";
+
+  function showAlert1() {
+   alert('Good fortune!');
+  }
+  function showAlert2() {
+    alert('Bad luck!');
+   }
+   function showAlert3() {
+    alert("Don't turn around");
+   }
